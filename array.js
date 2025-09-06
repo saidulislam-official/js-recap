7fc14ba09fee954a7132d046514fddbe26c04743
saidulislam-official <saidulislam.own@gmail.com>
2025-09-07 00:17:40 +0600

    array added

---
+++ array.js
@@ -0,0 +1,23 @@
+let fruits = ['apple', 'banana','coconut']
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+
+fruits[0] = 'mango';
+console.log(fruits);
+
+
+fruits[3] = 'orange';
+console.log(fruits);
+
+fruits.push("lime","lemon",'guava','fig',"cherry",'plum');
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+
+
