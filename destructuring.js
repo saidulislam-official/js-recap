bb1c893b7eafc873925245492f1a2a1a2b126b03
saidulislam-official <saidulislam.own@gmail.com>
2025-09-07 11:16:43 +0600

    added destructuring

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const arr = ["Steve", "Bill"];
+const [steve, bill] = 'arr';
+
+console.log(steve);
+console.log(bill);
+
+let a = "steve";
+let b = "bill";
+
+[a, b] = [b, a]
+
+console.log(a, b);
---
+++ mutable.js
@@ -0,0 +1,20 @@
+let name1 = "steve";
+let name2 = "bill";
+
+
+console.log(name1);
+console.log(name2);
+
+name2 = "elon";
+console.log(name1);
+console.log(name2);
+
+
+let person1 = {
+    name :"steve", age : 22
+};
+let person2 = person1;
+person2.name = "Bill";
+
+console.log(person1);
+console.log(person2);
