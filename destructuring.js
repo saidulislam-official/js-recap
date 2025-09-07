e4b5e40d5a63734b11b6bd8d6cb1147071a5458d
saidulislam-official <saidulislam.own@gmail.com>
2025-09-07 14:01:58 +0600

    added destructuring

--- destructuring.js
+++ destructuring.js
@@ -1,5 +1,5 @@
 const arr = ["Steve", "Bill"];
-const [steve, bill] = 'arr';
+const [steve, bill] = arr;
 
 console.log(steve);
 console.log(bill);
@@ -10,3 +10,63 @@ let b = "bill";
 [a, b] = [b, a]
 
 console.log(a, b);
+
+
+
+const colors = ["red", "blue", "green" , "black", "white"];
+
+console.log(colors);
+
+[colors[0], colors[4]] = [colors[4], colors[0]];
+
+console.log(colors);
+
+
+const [firstColor, secondColor, thirdColor] = colors;
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColor);
+
+
+const [fourthColor, ...extracolors] = colors;
+console.log(fourthColor)
+console.log(extracolors);
+
+
+
+
+// extract values from objects
+
+const person1 = {
+    firstName : "Steve",
+    lastName : "Bill",
+    age  : 65,
+    job : "founder",
+    
+}
+const person2 = {
+    firstName2 : "Elon",
+    lastName2 : "Musk",
+    age2  : 55,
+}
+// const {firstName, lastName, age, job}=person1;
+// console.log(firstName);
+// console.log(lastName);
+// console.log(age);
+// console.log(job);
+const {firstName2, lastName2, age2, job2="unemploye"} = person2;
+console.log(firstName2);
+console.log(lastName2);
+console.log(age2);
+console.log(job2);
+
+console.log("-------------------")
+
+// destructure in function parameters
+function displayPerson({firstName, lastName, age, job}){
+    console.log(`name :${firstName} ${lastName}`);
+    console.log(`age : ${age}`);
+    console.log(`job : ${job}`);
+}
+
+displayPerson(person1);
